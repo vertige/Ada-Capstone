a3837d61a8bf95b4b6e35f74ad7ea8856e295939
Jessica Owens <vertige@gmail.com>
2018-01-02 21:06:02 -0800

    Added Firebase config to environment files

--- ravelbot/src/environments/environment.ts
+++ ravelbot/src/environments/environment.ts
@@ -5,4 +5,13 @@
 
 export const environment = {
   production: false
+
+  firebase: {
+    apiKey: "AIzaSyD4l-j9wQgODTENNCQTX1CXeUHmHwyF9tY",
+    authDomain: "ravelbot.firebaseapp.com",
+    databaseURL: "https://ravelbot.firebaseio.com",
+    projectId: "ravelbot",
+    storageBucket: "ravelbot.appspot.com",
+    messagingSenderId: "44323616886"
+  }
 };

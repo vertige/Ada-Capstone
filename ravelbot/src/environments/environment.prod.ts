382a9145ca03a7abd2807e8e7eda8881621d513d
Jessica Owens <vertige@gmail.com>
2018-01-03 17:51:31 -0800

    Revert "Added Firebase config to environment files"
    This reverts commit a3837d61a8bf95b4b6e35f74ad7ea8856e295939.
    Undid messy firebase cloud functions initialization

--- ravelbot/src/environments/environment.prod.ts
+++ ravelbot/src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
   production: true
-
-  firebase: {
-    apiKey: "AIzaSyD4l-j9wQgODTENNCQTX1CXeUHmHwyF9tY",
-    authDomain: "ravelbot.firebaseapp.com",
-    databaseURL: "https://ravelbot.firebaseio.com",
-    projectId: "ravelbot",
-    storageBucket: "ravelbot.appspot.com",
-    messagingSenderId: "44323616886"
-  }
 };
--- ravelbot/src/environments/environment.ts
+++ ravelbot/src/environments/environment.ts
@@ -5,13 +5,4 @@
 
 export const environment = {
   production: false
-
-  firebase: {
-    apiKey: "AIzaSyD4l-j9wQgODTENNCQTX1CXeUHmHwyF9tY",
-    authDomain: "ravelbot.firebaseapp.com",
-    databaseURL: "https://ravelbot.firebaseio.com",
-    projectId: "ravelbot",
-    storageBucket: "ravelbot.appspot.com",
-    messagingSenderId: "44323616886"
-  }
 };
